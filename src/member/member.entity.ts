3ce23452274899608ca29471ece0b842e4550b30
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-04-29 23:31:24 +0900

    feat: Prisma 회원 모델 정의 및 Entity 생성

---
+++ src/member/member.entity.ts
@@ -0,0 +1,8 @@
+export class Member {
+    constructor(private id: number,
+                private email: string,
+                private nickname: string,
+                private password: string,
+                private profile: string) {
+    }
+}
