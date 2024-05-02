07ff849ab420d93f494e362dbfbca8b4f4bff105
Mingi Kim <migni4575@gmail.com>
2024-05-02 17:09:56 +0900

    fix: member entity Prisma 엔터티 구현하도록 수정

--- src/member/member.entity.ts
+++ src/member/member.entity.ts
@@ -1,5 +1,9 @@
-export class Member {
-    constructor(private id: number,
+import {
+    Member,
+} from "@prisma/client";
+
+export class MemberEntity implements Member {
+    constructor(private id: bigint,
                 private email: string,
                 private nickname: string,
                 private password: string,
